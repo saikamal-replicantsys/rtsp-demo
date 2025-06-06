1aeadc59d23a8ff2c1fc0a84d13561dd31576e34
Dantu Sai Kamal <saikamal@replicantsys.com>
2025-06-06 13:05:01 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install python3-gi python3-gi-cairo gir1.2-gst-rtsp-server-1.0 gstreamer1.0-tools \
+                 gstreamer1.0-plugins-base gstreamer1.0-plugins-good \
+                 gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
